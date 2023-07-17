a90b13a7afffc5c4395d778f91842d095262ebf9
zhengsongjun <zheng_songjun@venusgroup.com.cn>
2023-07-17 10:22:49 +0800

    feat:适配器改造一下servlet

---
+++ hotel/src/main/java/com/zhengsongjun/hotel/MyGenericServlet.java
@@ -0,0 +1,30 @@
+package com.zhengsongjun.hotel;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public abstract class MyGenericServlet implements Servlet {
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public abstract void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException;
+
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
---
+++ hotel/src/main/java/com/zhengsongjun/hotel/ServletDemo.java
@@ -0,0 +1,13 @@
+package com.zhengsongjun.hotel;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+
+public class ServletDemo extends MyGenericServlet{
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("实现业务逻辑");
+    }
+}
