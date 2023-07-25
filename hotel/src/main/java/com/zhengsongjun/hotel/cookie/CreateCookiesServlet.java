cad45d22063f34cf6b88eee0c9d70b5047b2ba83
zhengsongjun <zheng_songjun@venusgroup.com.cn>
2023-07-25 15:02:43 +0800

    feat: cookie 的path和Max-Age

--- hotel/src/main/java/com/zhengsongjun/hotel/cookie/CreateCookiesServlet.java
+++ hotel/src/main/java/com/zhengsongjun/hotel/cookie/CreateCookiesServlet.java
@@ -12,7 +12,10 @@ public class CreateCookiesServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Cookie cookie1 = new Cookie("name","zhengsongjun");
+        cookie1.setMaxAge(20);
         Cookie cookie2 = new Cookie("age","29");
+        cookie2.setPath(req.getContextPath());
+        cookie2.setMaxAge(100);
         resp.addCookie(cookie1);
         resp.addCookie(cookie2);
     }
