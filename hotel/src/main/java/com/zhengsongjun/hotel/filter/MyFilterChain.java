7956177f4d92d31ca4b78c6c9e3beec7f4f68eb2
zhengsongjun <zheng_songjun@venusgroup.com.cn>
2023-07-27 16:41:00 +0800

    feat: 改造filterchain代码，更加优雅

--- hotel/src/main/java/com/zhengsongjun/hotel/filter/MyFilterChain.java
+++ hotel/src/main/java/com/zhengsongjun/hotel/filter/MyFilterChain.java
@@ -1,40 +1,47 @@
 package com.zhengsongjun.hotel.filter;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class MyFilterChain implements FilterChain {
     private ArrayList<Filter> filterArrayList;
 
-    private int point;
+    private int point = 0;
 
-    private Servlet servlet;
 
 
     public MyFilterChain(ArrayList<Filter> filterArrayList,Servlet servlet){
-        this.filterArrayList = filterArrayList;
-        this.servlet = servlet;
-        if(filterArrayList != null && filterArrayList.size() > 0) {
-            this.point = 0;
-        }else {
-            this.point = -1;
+        if(filterArrayList == null) {
+            filterArrayList = new ArrayList<>();
         }
+        filterArrayList.add(new FilterProxy(servlet));
+        this.filterArrayList = filterArrayList;
     }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse) throws IOException, ServletException {
-        System.out.println(this.point);
-        if(this.point == -1) {
-            this.servlet.service(servletRequest,servletResponse);
-        }else{
-            if(this.point >= filterArrayList.size()){
-                servlet.service(servletRequest,servletResponse);
-            }else{
-                Filter filter = filterArrayList.get(point);
-                point++;
-                filter.doFilter(servletRequest,servletResponse,this);
-            }
+        if(this.point < filterArrayList.size()) {
+            Filter filter = filterArrayList.get(point);
+            point++;
+            filter.doFilter(servletRequest,servletResponse,this);
         }
     }
+
+    private static class FilterProxy extends HttpFilter {
+        private Servlet servlet;
+
+        public FilterProxy(Servlet servlet) {
+            this.servlet = servlet;
+        }
+
+        @Override
+        public void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+            servlet.service(req, res);
+        }
+    }
+
 }
